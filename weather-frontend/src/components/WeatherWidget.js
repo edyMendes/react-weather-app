7e13e493e7be6bf48e6ab54cb4ae972dd808355a
Edgar Filipe da Silva Mendes <76489842+edyMendes@users.noreply.github.com>
2024-08-18 15:24:23 +0100

    Creating the frontend for the authentication system

---
+++ weather-frontend/src/components/WeatherWidget.js
@@ -0,0 +1,33 @@
+// src/components/WeatherWidget.js
+import React from "react";
+
+const WeatherWidget = ({ city, data, unit }) => {
+  const unitSymbol =
+    unit === "metric" ? "°C" : unit === "imperial" ? "°F" : "K";
+
+  return (
+    <div className="weather-widget">
+      <h2 className="noselect">{city}</h2>
+      {data ? (
+        <div className="weather-details">
+          <div className="temperature noselect">
+            {data.temperature.toFixed(1)}
+            {unitSymbol}
+          </div>
+          <p className="noselect">
+            Max: {data.temp_max.toFixed(1)}
+            {unitSymbol} / Min: {data.temp_min.toFixed(1)}
+            {unitSymbol}
+          </p>
+          <div className="weather-description">
+            <p className="noselect">{data.weather_description}</p>
+          </div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default WeatherWidget;
